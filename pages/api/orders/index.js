e0404fd75c5740c60eea93ca7e0fa3b61f37c8fb
nawawis <nawawisboutique@gmail.com>
2022-08-08 23:56:09 +0700

    4 commit

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -4,9 +4,9 @@ import db from '../../../utils/db';
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
-  if (!session) {
-    return res.status(401).send('signin required');
-  }
+  // if (!session) {
+  //   return res.status(401).send('signin required');
+  // }
   const { user } = session;
   await db.connect();
   const newOrder = new Order({
