ba0192857138ca4df1739b6caac9855408e65d2d
nawawis <nawawisboutique@gmail.com>
2022-08-08 22:49:10 +0700

    4 commit

--- components/Layout.js
+++ components/Layout.js
@@ -92,7 +92,7 @@ export default function Layout({ title, children }) {
         </header>
         <main className="w-full m-auto mt-4 px-4">{children}</main>
         <footer className=" flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 Ali</p>
+          <p>Copyright © 2022 Ali Z</p>
         </footer>
       </div>
     </>
