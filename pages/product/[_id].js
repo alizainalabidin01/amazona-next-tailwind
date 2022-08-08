9fe05baac9c3d587d5750d075834b90a32c8f839
nawawis <nawawisboutique@gmail.com>
2022-08-08 23:00:29 +0700

    4 commit

--- pages/product/[_id].js
+++ pages/product/[_id].js
@@ -1,6 +1,6 @@
 // import { useRouter } from 'next/router';
 import React, { useContext } from 'react';
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import Link from 'next/link';
 import { Store } from '../../utils/Store';
 import db from '../../utils/db';
